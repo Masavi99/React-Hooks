3a021165968f829aff3fecfb2dfe8469c18d5c50
Masavi Syed <masavi.aiub@gmail.com>
2025-01-23 15:00:13 -0500

    spendgiftcard function

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import { useState } from "react";
 export default function App() {
   const [giftCard, setGiftCard] = useState(
     {
-        firstName: "Jennifer",
-        lastName: "Smith",
-        text: "Free dinner for 4 guests",
-        valid: true,
-        instructions: "To use your coupon, click the button below.",
+      firstName: "Jennifer",
+      lastName: "Smith",
+      text: "Free dinner for 4 guests",
+      valid: true,
+      instructions: "To use your coupon, click the button below.",
     }
   );
-
   function spendGiftCard() {
-
+    setGiftCard(prevState => {
+      return {
+        ...prevState,
+        text: "Your coupon has been used.",
+        valid: false,
+        instructions: "Please visit our restaurant to renew your gift card.",
+      }
+    });
   }
 
   return (
-    <div style={{padding: '40px'}}>
+    <div style={{ padding: '40px' }}>
       <h1>
         Gift Card Page
       </h1>
